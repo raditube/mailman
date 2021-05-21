a0c7612b82dfba5acb5cda0f56fb42db0980e989
evanzummeren <erikvanzummeren@gmail.com>
2021-05-21 17:17:03 -0400

    add logo

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,7 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <img src="./assets/raditube.svg" />
     </div>
     <router-view/>
   </div>
