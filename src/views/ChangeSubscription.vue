07534b06d80bbe97a4bc777bc9828316aeb2b735
evanzummeren <erikvanzummeren@gmail.com>
2021-05-21 17:07:13 -0400

    init

---
+++ src/views/ChangeSubscription.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="about">
+    <h1>{{subscription}}</h1>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'ChangeSubscription',
+  data: function () {
+    return {
+      subscription: {}
+    }
+  },
+  beforeMount() {
+    console.log(process.env);
+    axios.get(`https://${process.env.VUE_APP_BASE_API}/getSubscription/${this.$route.params.id}`)
+    .then((response) => {
+      this.subscription = response.data
+      console.log(response);
+    }, (error) => {
+      console.log(error);
+    });
+    // 
+  }
+
+}
+</script>
+
+<style>
+
+</style>
